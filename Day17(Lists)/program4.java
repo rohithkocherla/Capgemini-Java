aeefd26873c6f4344fff87a8fdc360c5f5ceed8a
rohit <rohithkocherla@gmail.com>
2026-01-18 23:21:17 +0530

    message

--- Day17(Lists)/program4.java
+++ Day17(Lists)/program4.java
@@ -18,6 +18,13 @@
             for(int i=0;i<al.size();i++){
                 System.out.println(al.get(i));
             }
+            System.out.println("----------");
+
+            Iterator<String> itr=al.iterator();
+            while(itr.hasNext()){
+                System.out.println(itr.next());
+                
+            }
             
     
         }
